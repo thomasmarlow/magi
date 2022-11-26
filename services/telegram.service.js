5fd9351d5f68e807193a2f6261b2edbe4bcfd4c7
Thomas Marlow <43114877+thomasmarlow@users.noreply.github.com>
2022-11-26 13:04:16 -0300

    log incoming messages

--- services/telegram.service.js
+++ services/telegram.service.js
@@ -106,6 +106,12 @@ module.exports = {
                 bot.on('message', (msg) => {
                         const chatId = msg.chat.id;
 
+                        // this.logger.info('incoming tg message', msg)
+                        this.logger.info('incoming tg message', {
+                                'chat': msg.chat,
+                                'text': msg.text
+                        })
+
                         // send a message to the chat acknowledging receipt of their message
                         bot.sendMessage(chatId, 'Received your message');
               });
