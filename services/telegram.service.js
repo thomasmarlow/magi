1038c6f1f128a8ba4b71225a4535f03d05abc156
Thomas Marlow <43114877+thomasmarlow@users.noreply.github.com>
2022-11-26 17:52:47 -0300

    fix prev commit

--- services/telegram.service.js
+++ services/telegram.service.js
@@ -45,7 +45,8 @@ module.exports = {
 			async handler(ctx) {
                                 this.bot.sendMessage(
                                     process.env.OWNER_TELEGRAM_CHAT_ID,
-                                    `${ctx.params.message}\n__fired by MAGI notifications__`
+                                    `${ctx.params.message}\n_fired by MAGI notifications_`,
+                                    { parseMode: 'Markdown' }
                                 )
 			}
 		},
